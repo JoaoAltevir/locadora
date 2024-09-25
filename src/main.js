70e6c58d34fd4f3da909cd15bcf9b5287ef21016
João Neto <j.altevirneto@gmail.com>
2024-09-25 10:36:12 -0300

    feat: add rotas de user e movie

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import "dotenv/config"
 import e from "express"
 import "./config/db.js"
-
+import movieRouter from "./routes/movie-router.js"
+import userRouter from "./routes/user-router.js"
 const app = e();
 
 
 app.use(e.json());
-
+app.use("/user", userRouter)
+app.use("/movie", movieRouter);
 app.listen(process.env.API_PORT , () => {
     console.log(`listening on port ${process.env.API_PORT}`)
 })
